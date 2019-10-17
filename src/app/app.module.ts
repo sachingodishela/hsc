594b981068d723ce7fa858af1de37b314f31f43e
Puneet2000 <cs17btech11029@iith.ac.in>
2019-10-17 18:26:26 +0530

    v1-material theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { RouterModule, Routes } from '@angular/router';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { StatementComponent } from './components/statement/statement.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { Statement } from '@angular/compiler';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ResearchComponent } from './components/research/research.component';
+import { ChallengesComponent } from './components/challenges/challenges.component';
+import { PolicyComponent } from './components/policy/policy.component';
+import { DataComponent } from './components/data/data.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'statement', component: StatementComponent },
+  { path: 'aboutus', component: AboutusComponent},
+  { path: 'data', component: DataComponent},
+  { path: 'research', component: ResearchComponent},
+  { path: 'challenge', component: ChallengesComponent},
+  { path: 'policy', component: PolicyComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StatementComponent,
+    AboutusComponent,
+    ResearchComponent,
+    ChallengesComponent,
+    PolicyComponent,
+    DataComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
